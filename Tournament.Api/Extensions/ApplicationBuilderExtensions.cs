1e70cc3f6efb3a1893fb897ccf0f2458dbf12e03
Vilith <vilith44@gmail.com>
2025-06-18 15:51:31 +0200

    Add project files.

---
+++ Tournament.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tournament.Data.Data;
+
+namespace Tournament.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder builder)
+        {
+            using var scope = builder.ApplicationServices.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<TournamentContext>();
+
+            await db.Database.MigrateAsync();
+
+            if (await db.TournamentDetails.AnyAsync())
+            {
+                // Seed initial data if the database is empty
+                return;
+            }
+
+            
+        }
+    }
+}
---
+++ Tournament.Data/Data/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Tournament.Core.Entities;
+
+namespace Tournament.Data.Data
+{
+    public static class SeedData
+    {
+        public static async Task InitAsync(TournamentContext context)
+        {
+            if (await context.TournamentDetails.AnyAsync())
+            {
+                return; // Database already seeded
+            }
+
+            var gameTitle = new[]
+            {
+                "League of Legends", "Dota 2", "Counter-Strike: Global Offensive",
+                "Valorant", "Overwatch", "Call of Duty", "FIFA", "PUBG", "Apex Legends", "Rainbow Six Siege",
+                "StarCraft II", "Hearthstone", "Fortnite", "Rocket League", "World of Warcraft"
+            };
+
+            var gameFaker = new Faker<Game>()
+                .RuleFor(g => g.Title, f => f.PickRandom(gameTitle))
+                .RuleFor(g => g.Time, f => f.Date.Future());
+
+            var tournamentFaker = new Faker<TournamentDetails>()
+                .RuleFor(t => t.Title, f => $"{f.Company.CompanyName()} Championship")
+                .RuleFor(t => t.StartDate, f => f.Date.Soon(30))
+                .RuleFor(t => t.Games, f => gameFaker.Generate(f.Random.Int(2, 5)));
+
+            var tournaments = tournamentFaker.Generate(3);
+
+            context.TournamentDetails.AddRange(tournaments);
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
