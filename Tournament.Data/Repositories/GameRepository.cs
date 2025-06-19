6b580a354bb24898a5c59052a436a96727a96fc9
Vilith <vilith44@gmail.com>
2025-06-19 13:48:36 +0200

    Added comments for checking if ok later on

--- Tournament.Data/Repositories/GameRepository.cs
+++ Tournament.Data/Repositories/GameRepository.cs
@@ -14,32 +14,31 @@ namespace Tournament.Data.Repositories
     {
         private readonly TournamentContext _context = context;
 
-        public async Task<IEnumerable<Game>> GetAllAsync()
+        public async Task<IEnumerable<Game>> GetAllAsync() /*=> await _context.Games.ToListAsync();*/
         {
             return await _context.Games.ToListAsync();
         }
 
-        public async Task<Game?> GetAsync(int id)
+        public async Task<Game?> GetAsync(int id) /*=> await _context.Games.FirstOrDefaultAsync(g => g.Id == id);*/
         {
             return await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
         }
 
-        public async Task<bool> AnyAsync(int id)
+        public async Task<bool> AnyAsync(int id) /*=> await _context.Games.AnyAsync(g => g.Id == id);*/
         {
             return await _context.Games.AnyAsync(g => g.Id == id);
         }
 
-        public void Add(Game game)
-        {
-            _context.Games.Add(game);
+        public void Add(Game game) /*=> _context.Games.Add(game);*/
+        { 
+            _context.Games.Add(game); 
         }
-
-        public void Update(Game game)
+        public void Update(Game game) /*=> _context.Games.Update(game);*/
         {
             _context.Games.Update(game);
         }
 
-        public void Remove(Game game)
+        public void Remove(Game game) /*=> _context.Games.Remove(game);*/
         {
             _context.Games.Remove(game);
         }
